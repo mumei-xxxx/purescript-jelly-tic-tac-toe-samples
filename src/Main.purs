624c3ac9cfff106b62d94c64144a4926c8616bdb
mumei <lesilluminations0@gmail.com>
2022-11-21 12:46:35 +0900

    .

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,23 @@ module Main where
 
 import Prelude
 
+import Data.Foldable (traverse_)
 import Effect (Effect)
-import Effect.Console (log)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
+import Jelly.Aff (awaitBody)
+import Jelly.Component (class Component, text)
+import Jelly.Element as JE
+import Jelly.Hooks (runHooks_)
+import Jelly.Hydrate (mount)
+import Jelly.Prop ((:=))
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = launchAff_ do
+  bodyMaybe <- awaitBody
+  liftEffect $ traverse_ (runHooks_ <<< mount component) bodyMaybe
+
+component :: forall m. Component m => m Unit
+component =
+  JE.div [ "class" := "w-screen h-screen text-5xl flex justify-center items-center" ] do
+    text "Hello Jelly!"
