44aff6ee7ff73af14e642e2f4c7f4c41fd2c7611
mumei <lesilluminations0@gmail.com>
2022-12-20 11:35:40 +0900

    .

--- purescript-calculatewinner-examples/src/Main.purs
+++ purescript-calculatewinner-examples/src/Main.purs
@@ -35,35 +35,57 @@ instance Show SquareValueType where
 
 type Board = Array (Maybe SquareValueType)
 
--- calculateWinner :: Board -> Maybe SquareValueType
--- calculateWinner boardArr =
---   let
--- | ある Line が同じ SquareValueType で埋まっているかどうか判定する
 {-
   https://pursuit.purescript.org/packages/purescript-arrays/7.1.0/docs/Data.Array#v:all
 -}
--- pred :: Array Int -> SquareValueType -> Boolean
--- pred line sv = all (\i -> boardArr !! i == Just (Just sv)) line
 
--- | すべての Line, SquareValueType の組み合わせについて pred を評価する
--- checked :: Array (Maybe SquareValueType)
--- checked = do
---   line <- lines
---   sv <- [ X, O ]
---   pure
---     if pred line sv then
---       Just sv
---     else
---       Nothing
--- in
---   join $ find isJust checked
--- | checked の中で一番最初に Just が出てきたものを返す
--- | find で帰ってくるのは Maybe (Maybe SquareValueType) なので、join で一つ unwrap する
 {-
   find https://pursuit.purescript.org/packages/purescript-arrays/7.1.0/docs/Data.Array#v:find
   isJust
   https://pursuit.purescript.org/packages/purescript-maybe/6.0.0/docs/Data.Maybe#v:isJust
 -}
+
+
+calculateWinner1 :: Board -> Maybe SquareValue
+calculateWinner1 boardArr =
+  let
+    -- | ある Line が同じ SquareValue で埋まっているかどうか判定する
+    pred :: Array Int -> SquareValue -> Boolean
+    pred line sv = all (\i -> boardArr !! i == Just (Just sv)) line
+
+    -- | すべての Line, SquareValue の組み合わせについて pred を評価する
+    checked :: Array (Maybe SquareValue)
+    checked = do
+      line <- lines
+      sv <- [ X, O ]
+      pure
+        if pred line sv then
+          Just sv
+        else
+          Nothing
+  in
+    -- | checked の中で一番最初に Just が出てきたものを返す
+    -- | find で帰ってくるのは Maybe (Maybe SquareValue) なので、join で一つ unwrap する
+    join $ find isJust checked
+
+
+calculateWinner2 :: Board -> Maybe SquareValue
+calculateWinner2 boardArr =
+  let
+    -- | ある Line が同じ SquareValue で埋まっているかどうか判定する
+    pred :: Array Int -> SquareValue -> Boolean
+    pred line sv = all (\i -> boardArr !! i == Just (Just sv)) line
+
+    -- | すべての Line, SquareValue の組み合わせについて pred を評価する
+    -- | false ならそれはスキップする
+    checked :: Array SquareValue
+    checked = do
+      line <- lines
+      sv <- [ X, O ]
+      guard $ pred line sv
+      pure sv
+  in
+    head checked
 
 {-
   [X, O, Nothing, X, O, O, X, O, O]
